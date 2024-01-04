df8d5f6101387c61dad80b67d1147ae59eb3c446
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-01-04 16:25:10 +0200

    website completed

--- src/js/siema.js
+++ src/js/siema.js
@@ -14,3 +14,24 @@ const mySiema = new Siema({
 });
 document.querySelector(".prev").addEventListener("click", () => mySiema.prev());
 document.querySelector(".next").addEventListener("click", () => mySiema.next());
+
+const mySiemaTwo = new Siema({
+  selector: ".siema-reviews",
+  duration: 200,
+  easing: "ease-out",
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
+document
+  .querySelector(".prev-reviews")
+  .addEventListener("click", () => mySiemaTwo.prev());
+document
+  .querySelector(".next-reviews")
+  .addEventListener("click", () => mySiemaTwo.next());
