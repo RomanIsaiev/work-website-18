e5c7d2a7c7843b0d2c3b31c937989006218770ff
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-01-05 13:51:53 +0200

    Update fixedButton.js

--- src/js/fixedButton.js
+++ src/js/fixedButton.js
@@ -1,6 +1,9 @@
 window.onscroll = function () {
   var button = document.getElementById("fixedButton");
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
     button.style.display = "block";
   } else {
     button.style.display = "none";
